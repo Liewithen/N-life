c3979f6200918493b7d49454688f1e5bb4c0ff78
yangsuper666 <yangsuper666@gmail.com>
2016-08-21 01:10:00 +0000

    更改数据库配置和修改公选任选问题

--- mySite/settings.py
+++ mySite/settings.py
@@ -75,10 +75,8 @@ WSGI_APPLICATION = 'mySite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'JWC',
-        'USER': 'root',
-        'PASSWORD': '123456',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
